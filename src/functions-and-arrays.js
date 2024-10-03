2e8d58274ad6b1772c38c8f654e4cc0975d67b47
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-03 17:20:09 +0200

    first lab functions and arrays done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,58 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+if(a>b){
+    return a ;
+} else if (b>a){
+    return b;
+} else {
+    return a ;
+}
 
-
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    
+    if(arr.length === 0 ){
+        return null ;
+    } else {
 
+        let arrLengths = [];
+  
+        for (let i = 0; i < arr.length; i++) {
+          arrLengths.push(arr[i].length);
+          console.log(arrLengths);
+        }
+        console.log(arrLengths);
+      
+        const max = Math.max.apply(null, arrLengths);
+        //console.log(max);
+        const indexMax = arrLengths.indexOf(max);
+        //console.log(indexMax);
+        return arr[indexMax];
+    }
+    
+   
 
+  }
+
+console.log(Math.max(arrLengths));
+console.log(findLongestWord(words))
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers){
+    let result = 0;
+    for (let i=0; i < numbers.length; i++){
+        result += numbers[i];
+    }
+ return result
+}
 
 
 
@@ -23,7 +60,15 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers (arrayOfNumbers) {
+    let averageResult;
+    if (arrayOfNumbers.length === 0) {
+        averageResult = 0;
+    } else {
+        averageResult = (sumNumbers(arrayOfNumbers) / arrayOfNumbers.length);
+    }
+    return averageResult;
+}
 
 
 
@@ -31,4 +76,20 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, word) {
+    let result = true;
+  
+    if (arrayOfWords.length === 0) {
+      return null;
+    } else {
+      for (let k = 0; k < arrayOfWords.length; k++) {
+        if (arrayOfWords[k] === word) {
+          result = true;
+          break;
+        } else {
+          result = false;
+        }
+      }
+      return result;
+    }
+  }
